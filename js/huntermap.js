7024eb5af4a2a37262e017d6148bceb28b79b8b4
stpe <stefpet@gmail.com>
2010-09-08 16:07:34 +0200

    Restructured implementation...

--- js/huntermap.js
+++ js/huntermap.js
@@ -1,133 +1,53 @@
-window.theHunter = window.theHunter || {};
-theHunter.maps = theHunter.maps || {};
+var HMap = HMap || {};
 
-(function() {
-    theHunter.maps.toLatLng = function(p) {
-        return new google.maps.LatLng(-p.y / 16384 * 0.08789, p.x / 16384 * 0.08789);
-    };
+HMap.map = (function() {
 
-    theHunter.maps.toPoint = function(ll) {
-        return new google.maps.Point(Math.round(-ll.lat() / 0.08789 * 16384), Math.round(ll.lng() / 0.08789 * 16384));
-    };
+	var gmap;
+	
+	return {
+		init: function(div, options) {
+	        options = options || {};
+	        
+	        var center = (options.center) ? true : false;
+	        options.disableDefaultUI = true;
+	        options.navigationControl = true;
+	        options.scaleControl = false;
+	
+	        gmap = new google.maps.Map(div, options);
+	
+			HMap.mapTypes.set(gmap);
+	
+	        if (!center) {
+	            gmap.fitBounds(HMap.locations.EVERGREEN.Reserves.WHITEHARTISLAND.bounds);
+	        }
 
-    theHunter.maps.MarkerTypeId = {
-        LODGE: 0,
-        CAMPING: 1,
-        TOWER: 2
-    };
+	        HMap.markers.populateMarkers();
+	        
+	        google.maps.event.addListener(gmap, 'zoom_changed', HMap.markers.updateMarkers);
+	
+	        return gmap;
+		},
+		
+		getMap: function() {
+			return gmap;
+		},
+		
+		toLatLng: function(point) {
+			return new google.maps.LatLng(-point.y / 16384 * 0.08789, point.x / 16384 * 0.08789);
+		},
+		
+		toPoint: function(latLng) {
+	        return new google.maps.Point(Math.round(-latLng.lat() / 0.08789 * 16384), Math.round(latLng.lng() / 0.08789 * 16384));	
+		}
+	};
+})();
 
-    theHunter.maps.MarkerTypeImages = {};
-    theHunter.maps.MarkerTypeImages[theHunter.maps.MarkerTypeId.LODGE] = new google.maps.MarkerImage("gfx/Map-Icons-32x32.png",
-        new google.maps.Size(32, 32),
-        new google.maps.Point(64, 0),
-        new google.maps.Point(16, 16)
-    );
-    theHunter.maps.MarkerTypeImages[theHunter.maps.MarkerTypeId.CAMPING] = new google.maps.MarkerImage("gfx/Map-Icons-32x32.png",
-        new google.maps.Size(32, 32),
-        new google.maps.Point(160, 0),
-        new google.maps.Point(16, 16)
-    );
-    theHunter.maps.MarkerTypeImages[theHunter.maps.MarkerTypeId.TOWER] = new google.maps.MarkerImage("gfx/Map-Icons-32x32.png",
-        new google.maps.Size(32, 32),
-        new google.maps.Point(128, 0),
-        new google.maps.Point(16, 16)
-    );
 
-    theHunter.maps.Marker = function(title, type, point) {
-        this.title = name;
-        this.type = type;
-        this.point = point;
-    };
-
-    theHunter.maps.Marker.prototype.getMarker = function() {
-        return new google.maps.Marker({
-            title: this.title,
-            icon: theHunter.maps.MarkerTypeImages[this.type],
-            position: theHunter.maps.toLatLng(this.point)
-        });
-    };
-
-    theHunter.maps.Reserves = {
-        EVERGREEN: {
-            Reserves: {
-                WHITEHARTISLAND: {
-                    bounds: new google.maps.LatLngBounds(
-                        new google.maps.LatLng(-0.013, -0.085),
-                        new google.maps.LatLng(-0.053, -0.066)
-                    ),
-                    places: {
-                        LODGES: [
-                            new theHunter.maps.Marker(
-                                "The Causeway",
-                                theHunter.maps.MarkerTypeId.LODGE,
-                                new google.maps.Point(-13292, 3512)
-                            ),
-                            new theHunter.maps.Marker(
-                                "South Cliff Lodge",
-                                theHunter.maps.MarkerTypeId.LODGE,
-                                new google.maps.Point(-13661, 8645)
-                            )
-                        ],
-                        CAMPING: [
-
-                        ],
-                        TOWER: [
-
-                        ]
-                    }
-                },
-                LOGGERSPOINT: {
-                    bounds: new google.maps.LatLngBounds(
-                        new google.maps.LatLng(-0.023, -0.063),
-                        new google.maps.LatLng(-0.051, -0.038)
-                    ),
-                    places: {
-                        LODGES: [
-                            new theHunter.maps.Marker(
-                                "Logger's Point Field Lodge",
-                                theHunter.maps.MarkerTypeId.LODGE,
-                                new google.maps.Point(-10387, 7916)
-                            ),
-                            new theHunter.maps.Marker(
-                                "Logger's Point Border Lodge",
-                                theHunter.maps.MarkerTypeId.LODGE,
-                                new google.maps.Point(-8175, 5229)
-                            )
-                        ],
-                        CAMPING: [
-
-                        ],
-                        TOWER: [
-
-                        ]
-                    }
-                }
-            }
-        }
-    };
-
-    for (var r in theHunter.maps.Reserves) {
-        var b = new google.maps.LatLngBounds();
-        var reserve = theHunter.maps.Reserves[r];
-        for (var r2 in reserve.Reserves) {
-            b.union(reserve.Reserves[r2].bounds);
-        }
-        reserve.bounds = b;
-    }
-
-    theHunter.maps.addMarkers = function(markers, map) {
-        for (var m in markers) {
-            var marker = markers[m].getMarker();
-            marker.setMap(map);
-        }
-    };
-
-    theHunter.maps.MapTypeId = {
-        SATELLITE: "theHunter_satellite"
-    };
-
-    var mapTypes = {};
-    mapTypes[theHunter.maps.MapTypeId.SATELLITE] = new google.maps.ImageMapType({
+HMap.mapTypes = (function() {
+	
+	var types = {};
+	
+    types[HMap.mapTypeId.SATELLITE] = new google.maps.ImageMapType({
         name: 'theHunter Satellite',
         getTileUrl: function(coord, zoom) {
             var z = zoom - 11;
@@ -152,28 +72,128 @@ theHunter.maps = theHunter.maps || {};
         minZoom: 11,
         maxZoom: 16
     });
+	
+	return {
+		set: function(gmap) {
+	        for (var k in types) {
+	            gmap.mapTypes.set(k, types[k]);
+	        }
+	        gmap.setMapTypeId(HMap.mapTypeId.SATELLITE);
+		}
+	};
+	
+	
+})();
 
-    theHunter.maps.getMap = function(div, options) {
-        options = options || {};
-        var center = (options.center) ? true : false;
-        options.disableDefaultUI = true;
-        options.navigationControl = true;
-        options.scaleControl = false;
 
-        var m = new google.maps.Map(div, options);
+HMap.markers = (function() {
 
-        for (var k in mapTypes) {
-            m.mapTypes.set(k, mapTypes[k]);
-        }
-        m.setMapTypeId(theHunter.maps.MapTypeId.SATELLITE);
-
-        if (!center) {
-            m.fitBounds(theHunter.maps.Reserves.EVERGREEN.bounds);
-        }
-
-        return m;
+	var markers = [];
+	var previousZoomLevel;
+	var types = {};
+	
+	types[HMap.markerTypeId.LODGE] = {
+		0: 	new google.maps.MarkerImage(
+				"gfx/Map-Icons-32x32.png",
+		        new google.maps.Size(32, 32),
+		        new google.maps.Point(64, 0),
+		        new google.maps.Point(16, 16)
+    		),
+		1: 	new google.maps.MarkerImage(
+				"gfx/Map-Icons-16x16.png",
+		        new google.maps.Size(16, 16),
+		        new google.maps.Point(32, 0),
+		        new google.maps.Point(8, 8)
+    		)
+	};
+	
+    types[HMap.markerTypeId.CAMPSITE] = {
+    	0: new google.maps.MarkerImage(
+		    	"gfx/Map-Icons-32x32.png",
+		        new google.maps.Size(32, 32),
+		        new google.maps.Point(160, 0),
+		        new google.maps.Point(16, 16)
+		    ),
+    	1: new google.maps.MarkerImage(
+				"gfx/Map-Icons-16x16.png",
+		        new google.maps.Size(16, 16),
+		        new google.maps.Point(80, 0),
+		        new google.maps.Point(8, 8)
+		    )
     };
-
-    // theHunter.maps.Map.prototype.construtor = theHunter.maps.Map;
-    // theHunter.maps.Map.prototype = new google.maps.Map();
+    
+    types[HMap.markerTypeId.TOWER] = {
+    	0: new google.maps.MarkerImage(
+		    	"gfx/Map-Icons-32x32.png",
+		        new google.maps.Size(32, 32),
+		        new google.maps.Point(128, 0),
+		        new google.maps.Point(16, 16)    
+		    ),
+    	1: new google.maps.MarkerImage(
+				"gfx/Map-Icons-16x16.png",
+		        new google.maps.Size(16, 16),
+		        new google.maps.Point(64, 0),
+		        new google.maps.Point(8, 8)  
+		    )
+    };
+    
+    types[HMap.markerTypeId.POI] = {
+    	0: new google.maps.MarkerImage(
+			 	"gfx/Map-Icons-32x32.png",
+		        new google.maps.Size(32, 32),
+		        new google.maps.Point(192, 0),
+		        new google.maps.Point(16, 16)    
+		   ),
+    	1: new google.maps.MarkerImage(
+				"gfx/Map-Icons-16x16.png",
+		        new google.maps.Size(16, 16),
+		        new google.maps.Point(96, 0),
+		        new google.maps.Point(8, 8)   
+		   )
+    };    
+    
+    var getImageZoomLevel = function(zoom) {
+    	return zoom < 15 ? 1 : 0;
+    };
+        
+    return {    	
+		populateMarkers: function() {
+			var map = HMap.map.getMap();
+			var zoom = map.getZoom();
+			previousZoomLevel = zoom;
+			var imageZoomLevel = getImageZoomLevel(zoom);
+			
+			for (var i = 0; i < HMap.worldObjects.length; i++) {
+				var obj = HMap.worldObjects[i];
+				var marker =  new google.maps.Marker({
+	    			title: obj.title,
+	    			icon: types[obj.type][imageZoomLevel],
+	    			position: HMap.map.toLatLng(new google.maps.Point(obj.map_x, obj.map_z)),
+	    			map: map
+	    		});
+	    		marker._obj = obj;
+	    		
+	    		markers.push(marker);
+			}
+		},
+    	
+    	/**
+    	 * Update markers on zoom change.
+    	 */
+    	updateMarkers: function() {
+    		var zoom = HMap.map.getMap().getZoom();
+    		var imageZoomLevel = getImageZoomLevel(zoom);
+    		
+    		if (imageZoomLevel != getImageZoomLevel(previousZoomLevel)) {
+	    		for (var i = 0; i < markers.length; i++) {
+	    			markers[i].setIcon(types[markers[i]._obj.type][imageZoomLevel]);
+	    		}
+	    	}
+	    	
+	    	previousZoomLevel = zoom;
+    	},
+    	
+    	types: types
+    }
+    
 })();
